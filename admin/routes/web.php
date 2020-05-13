c3958d91069a56a64153e3f6b1152abe44f04dbf
Faisal Abdul Slam <36113693+Faisal0385@users.noreply.github.com>
2020-05-14 00:43:32 +0600

    Laravel Portfolio Website V1
    Update-2
    Laravel Portfolio Website V1

--- admin/routes/web.php
+++ admin/routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@HomeIndex');
+
+Route::get('/visitor', 'VisitorController@VisitorIndex');
